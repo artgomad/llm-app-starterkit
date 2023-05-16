fc853f9be301a6ec7cca1a3a96cb2fe5b28e6c21
Tim van de Vathorst <tim@vandevathorst.nl>
2023-05-16 15:21:07 +0200

    feature(ctrl-c): make the exit process functionality work

--- backend/app/api.py
+++ backend/app/api.py
@@ -6,10 +6,12 @@ import os
 import json
 from app.chains.BasicChatChain import BasicChatChain
 import asyncio
-
-app = FastAPI()
+import signal
 
 load_dotenv()
+
+app = FastAPI()
+exit_flag = False
 openai.api_key = os.environ.get('OPENAI_API_KEY')
 
 # Define which front-end origins are allowed to make requests
@@ -25,7 +27,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"]
 )
-
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
@@ -49,8 +50,8 @@ async def websocket_endpoint(websocket: WebSocket):
         chat_chain = BasicChatChain.create_chain()
 
         llm_response = chat_chain.run(
-            {'system_message': system_message, 
-             'user_message_template': user_message_template, 
+            {'system_message': system_message,
+             'user_message_template': user_message_template,
              'chat_history': chatlog_strings})
 
         print('llm response = ')
@@ -59,3 +60,27 @@ async def websocket_endpoint(websocket: WebSocket):
         await websocket.send_json({
             "data":  llm_response,
         })
+
+# Register a signal handler for SIGINT (Ctrl-C)
+def handle_exit_signal(signum, frame):
+    global exit_flag
+    exit_flag = True
+
+@app.on_event("startup")
+def startup_event():
+    # Start a task to check the exit flag periodically
+    asyncio.create_task(check_exit_flag())
+
+async def check_exit_flag():
+    while not exit_flag:
+        await asyncio.sleep(0.1)  # Adjust the sleep duration as needed
+
+    # Perform any cleanup tasks or additional shutdown logic here
+    print("Exiting gracefully...")
+    # Optionally, you can close any open connections or perform cleanup tasks
+
+    # Stop the application server
+    await app.stop()
+
+# Attach the signal handler to the SIGINT signal
+signal.signal(signal.SIGINT, handle_exit_signal)
