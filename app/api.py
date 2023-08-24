5c6bbde091f8de09942e78e99df423d89379906e
Pablo Ferreras <p.ferreras@talespin.company>
2023-08-24 17:24:18 +0200

    Starting to simplify the code

--- app/api.py
+++ app/api.py
@@ -12,6 +12,7 @@ import asyncio
 import signal
 from app.chains.BasicChatChain import basicOpenAICompletion
 from app.utils.vectorstores.Faiss import Faiss
+from app.utils.functions.search_products_based_on_profile import search_products_based_on_profile
 
 
 load_dotenv()
@@ -128,7 +129,7 @@ async def websocket_endpoint(websocket: WebSocket):
                 await websocket.send_json({
                     "error": error_message,
                     "error_traceback": last_5_lines_tb,
-                    "context":  returned_context,
+                    "context":  "Error when updating the customer profile.",
                 })
 
         # 02 CORE GPT FUNCTION ROUTER
@@ -157,10 +158,10 @@ async def websocket_endpoint(websocket: WebSocket):
                 # print(function_call_output.arguments)
                 # context_for_LLM = function_call_output.arguments
                 print('Calling a function!')
+                print(function_call_output['name'])
 
                 # With this function I want to return the basic content of all products that match the search terms
                 if function_call_output['name'] == 'search_food_products':
-                    print(function_call_output['name'])
                     # Transform the arguments property from a string to JSON
                     arguments = json.loads(function_call_output['arguments'])
                     field = arguments.get('field', "")
@@ -176,7 +177,6 @@ async def websocket_endpoint(websocket: WebSocket):
 
                 # With this function I want to return all the metadata of a single product
                 elif function_call_output['name'] == 'read_product_details':
-                    print(function_call_output['name'])
                     arguments = json.loads(function_call_output['arguments'])
                     searchQuery = arguments.get('product_name', "")
 
@@ -192,31 +192,9 @@ async def websocket_endpoint(websocket: WebSocket):
                     print('All Product info = ')
                     print(context_for_LLM)
                 elif function_call_output['name'] == 'search_products_based_on_profile':
-                    # After updating the profile we want to add to the context the matching plan from the database
-                    print(function_call_output['name'])
-                    searchQuery = json.dumps(customer_profile_update)
+                    all_product_info, context_for_LLM = search_products_based_on_profile(
+                        customer_profile_update, knowledge_base, score_threshold)
 
-                    faiss = Faiss(file_name=knowledge_base)
-                    all_product_info, context_for_LLM = faiss.vector_search(
-                        query=searchQuery, number_of_outputs=5)
-
-                    print("score_threshold")
-                    print(score_threshold)
-
-                    # Remove all the products with a score higher than 0.5
-                    all_product_info = [
-                        doc for doc in all_product_info if doc['metadata']['score'] < score_threshold]
-
-                    print("filtered products")
-                    print(all_product_info)
-
-                    context_for_LLM = "\n\n".join(
-                        f"Full product information: {json.dumps(doc['metadata'], indent=2)}"
-                        for doc in all_product_info
-                    )
-
-                    print('All Product info = ')
-                    print(context_for_LLM)
                 else:
                     print('function without effect')
 
---
+++ app/utils/functions/search_products_based_on_profile.py
@@ -0,0 +1,33 @@
+# Add to the context the matching plan from the database
+import json
+from app.utils.vectorstores.Faiss import Faiss
+
+
+def search_products_based_on_profile(customer_profile_update, knowledge_base, score_threshold):
+
+    searchQuery = json.dumps(customer_profile_update)
+
+    faiss = Faiss(file_name=knowledge_base)
+
+    all_product_info, context_for_LLM = faiss.vector_search(
+        query=searchQuery, number_of_outputs=5)
+
+    print("score_threshold")
+    print(score_threshold)
+
+    # Remove all the products with a score higher than 0.5
+    all_product_info = [
+        doc for doc in all_product_info if doc['metadata']['score'] < score_threshold]
+
+    print("filtered products")
+    print(all_product_info)
+
+    context_for_LLM = "\n\n".join(
+        f"Full product information: {json.dumps(doc['metadata'], indent=2)}"
+        for doc in all_product_info
+    )
+
+    print('All Product info = ')
+    print(context_for_LLM)
+
+    return all_product_info, context_for_LLM
