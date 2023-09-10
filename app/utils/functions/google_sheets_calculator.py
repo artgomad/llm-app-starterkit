5617b6091aa686d9cc64222ed8786534f6db85bd
Pablo Ferreras <p.ferreras@talespin.company>
2023-09-10 11:30:30 +0200

    debug

--- app/utils/functions/google_sheets_calculator.py
+++ app/utils/functions/google_sheets_calculator.py
@@ -122,6 +122,10 @@ def google_sheets_operations(creds, function_output):
 
         for row_idx in range(2, len(updated_table)):
             obj_row = updated_table[row_idx]
+            for col_idx in object_attribute_columns:
+                print(f'col_idx: {col_idx}')
+                print(f'obj_row: {obj_row}')
+                print(f'updated_table[1]: {updated_table[1]}')
             obj = {updated_table[1][col_idx]: obj_row[col_idx]
                    for col_idx in object_attribute_columns if obj_row[col_idx]}
             selected_objects.append(obj)
