b9328fc3b6d2a84903e06f4e0b0c0f95357e8321
Pablo Ferreras <p.ferreras@talespin.company>
2023-10-25 11:31:55 +0200

    Updated definitions of the folder directory

--- definitions.py
+++ definitions.py
@@ -1,11 +1,13 @@
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(ROOT_DIR)
 
-DATA_PATH = os.path.join(ROOT_DIR, 'data') 
+DATA_PATH = os.path.join(PROJECT_ROOT, 'data')
 
-CSV_FOLDER = os.path.join(DATA_PATH,'csv_files/')
-VECTORSTORE_FOLDER = os.path.join(DATA_PATH,'data/vectorstores/')
+CSV_FOLDER = os.path.join(DATA_PATH, 'csv_files/')
+VECTORSTORE_FOLDER = os.path.join(
+    DATA_PATH, 'vectorstores/')  # 'data/vectorstores/'
 
 # Create the folders if they don't exist
 os.makedirs(CSV_FOLDER, exist_ok=True)
