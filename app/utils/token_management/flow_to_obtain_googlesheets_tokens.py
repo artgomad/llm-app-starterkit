80b71a25c2a5adf6b42fa73b724d16c7812e8e8b
Pablo Ferreras <p.ferreras@talespin.company>
2023-11-15 21:34:00 +0100

    Updated documentation of the working solution

--- app/utils/token_management/flow_to_obtain_googlesheets_tokens.py
+++ app/utils/token_management/flow_to_obtain_googlesheets_tokens.py
@@ -1,3 +1,11 @@
+# This code is a Python script for obtaining OAuth 2.0 credentials and tokens
+# from a user to access the Google Sheets API.
+# By running this script, a user grants the application permission
+# to access and manage their Google Sheets through the API.
+# The credentials are saved locally in client_secret.json and token.json
+# Which can be uploaded to Heroku Config Vars manually (GOOGLE_CLIENT_SECRET and GOOGLE_SHEETS_TOKENS)
+# to enable the app to access the Google Sheets API.
+
 import os
 from google_auth_oauthlib.flow import InstalledAppFlow
 
